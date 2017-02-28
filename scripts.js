1361c3107a4554a8b783321fc47651685004520f
lotsofluck4m <lotsofluck4m@gmail.com>
2017-02-28 15:59:15 +0900

    123

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
-function go_search(keyword){
+function go_search(keyword) {
 
-    if ( !keyword ) {
+    if (!keyword) {
         return;
     }
 
     document.getElementById('unifiedSearchBox').setAttribute('value', keyword);
     document.getElementById('unified_search_form').submit();
+}
+
+function go_view(url, seq) {
+    document.location.href = url + "?srh[view_mode]=detail&srh[seq]=" + seq;
 }
