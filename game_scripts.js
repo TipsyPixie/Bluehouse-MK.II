a52be64d3e4555c1dd91f59f395c1e895cddaf4d
seong627 <lotsofluck4m@gmail.com>
2017-03-04 02:15:13 +0900

    candle sprites

--- game_scripts.js
+++ game_scripts.js
@@ -1,13 +1,30 @@
-let painter = {
+const painter = {
     canvasId: "Gwanghwamun",
+
     backgroundSrc: "res/looping_background.jpg",
+
     spriteSrc: "res/candle_sprite.png",
     sprites: [],
-    spriteWidth: 20,
-    spriteHeight: 20,
-    numberOfSprites: 9,
+    spriteWidth: 30,
+    spriteHeight: 60,
+    numberOfSprites: 15,
 
-    getCanvasSize: function (rect) {
+    playerSrc: "res/president_portrait.png",
+    playerWidth: 50,
+    playerHeight: 50,
+    direction: Object.freeze({
+        DOWN_LEFT: 1,
+        DOWN: 2,
+        DOWN_RIGHT: 3,
+        LEFT: 4,
+        NEUTRAL: 5,
+        RIGHT: 6,
+        UP_LEFT: 7,
+        UP: 8,
+        UP_RIGHT: 9
+    }),
+
+    getSize: function (rect) {
 
         this.width = rect.width;
         this.height = rect.height;
@@ -27,7 +44,78 @@ let painter = {
         sprite.positionY += scrollAmount * sprite.velocityScale;
 
         if (sprite.positionY > this.height) {
-            sprite.positionY = this.spriteWidth * 3 * (Math.random() - 1);
+            this.replaceSprite(sprite);
+
+            if (sprite == this.sprites[0]) {
+                sprite.positionX = this.playerX;
+                sprite.velocityScale = 0.75;
+            }
+        }
+    },
+
+    replaceSprite: function (sprite) {
+
+        sprite.positionY = -this.height * Math.random();
+        sprite.positionX = this.width * Math.random();
+        sprite.velocityScale = 0.5 + Math.random() * 1.5;
+    },
+
+    movePlayer: function (moveAmount) {
+
+        let currentDirection =
+            (keyMap[directionKey.UP] && keyMap[directionKey.LEFT] && this.direction.UP_LEFT) ||
+            (keyMap[directionKey.UP] && keyMap[directionKey.RIGHT] && this.direction.UP_RIGHT) ||
+            (keyMap[directionKey.DOWN] && keyMap[directionKey.LEFT] && this.direction.DOWN_LEFT) ||
+            (keyMap[directionKey.DOWN] && keyMap[directionKey.RIGHT] && this.direction.DOWN_RIGHT) ||
+            (keyMap[directionKey.UP] && this.direction.UP) ||
+            (keyMap[directionKey.DOWN] && this.direction.DOWN) ||
+            (keyMap[directionKey.LEFT] && this.direction.LEFT) ||
+            (keyMap[directionKey.RIGHT] && this.direction.RIGHT) ||
+            this.direction.NEUTRAL;
+
+        switch (currentDirection) {
+            case this.direction.NEUTRAL:
+                break;
+            case this.direction.LEFT:
+                this.playerX -= moveAmount;
+                if (this.playerX < 0) this.playerX = 0;
+                break;
+            case this.direction.UP:
+                this.playerY -= moveAmount;
+                if (this.playerY < 0) this.playerY = 0;
+                break;
+            case this.direction.RIGHT:
+                this.playerX += moveAmount;
+                if (this.playerX > this.width - this.playerWidth) this.playerX = this.width - this.playerWidth;
+                break;
+            case this.direction.DOWN:
+                this.playerY += moveAmount;
+                if (this.playerY > this.height - this.playerHeight) this.playerY = this.height - this.playerHeight;
+                break;
+            case this.direction.UP_LEFT:
+                this.playerX -= moveAmount / Math.sqrt(2);
+                this.playerY -= moveAmount / Math.sqrt(2);
+                if (this.playerX < 0) this.playerX = 0;
+                if (this.playerY < 0) this.playerY = 0;
+                break;
+            case this.direction.UP_RIGHT:
+                this.playerX += moveAmount / Math.sqrt(2);
+                this.playerY -= moveAmount / Math.sqrt(2);
+                if (this.playerX > this.width - this.playerWidth) this.playerX = this.width - this.playerWidth;
+                if (this.playerY < 0) this.playerY = 0;
+                break;
+            case this.direction.DOWN_LEFT:
+                this.playerX -= moveAmount / Math.sqrt(2);
+                this.playerY += moveAmount / Math.sqrt(2);
+                if (this.playerX < 0) this.playerX = 0;
+                if (this.playerY > this.height - this.playerHeight) this.playerY = this.height - this.playerHeight;
+                break;
+            case this.direction.DOWN_RIGHT:
+                this.playerX += moveAmount / Math.sqrt(2);
+                this.playerY += moveAmount / Math.sqrt(2);
+                if (this.playerX > this.width - this.playerWidth) this.playerX = this.width - this.playerWidth;
+                if (this.playerY > this.height - this.playerHeight) this.playerY = this.height - this.playerHeight;
+                break;
         }
     },
 
@@ -36,7 +124,7 @@ let painter = {
         let backgroundImage = new Image();
         backgroundImage.src = this.backgroundSrc;
         backgroundImage.style.height = this.height;
-        backgroundImage.style.zIndex = 0;
+        backgroundImage.style.zIndex = 1;
 
         return backgroundImage;
     },
@@ -45,69 +133,188 @@ let painter = {
 
         let spriteImage = new Image();
         spriteImage.src = this.spriteSrc;
-        spriteImage.style.zIndex = 1;
+        spriteImage.style.zIndex = 3;
 
         return spriteImage;
     },
 
+    createPlayerImage: function () {
+
+        let playerImage = new Image();
+        playerImage.src = this.playerSrc;
+        playerImage.style.zIndex = 2;
+
+        return playerImage;
+    },
+
     initialize: function () {
 
-        this.field = document.getElementById(this.canvasId);
+        this.canvas = document.getElementById(this.canvasId);
 
-        if (!this.field.getContext("2d")) {
+        if (!this.canvas.getContext("2d")) {
             return;
         }
-        this.context = this.field.getContext("2d");
+        this.context = this.canvas.getContext("2d");
 
-        this.getCanvasSize(this.field);
+        this.getSize(this.canvas);
 
-        this.backgroundY = 0;
+        this.initBackground();
 
         this.initSprites();
 
+        this.initPlayer();
+
+        this.timer = 0;
+
+        keyMap[directionKey.UP] = false;
+        keyMap[directionKey.DOWN] = false;
+        keyMap[directionKey.LEFT] = false;
+        keyMap[directionKey.RIGHT] = false;
+
         // console.log("init");
+    },
+
+    initBackground: function () {
+
+        this.backgroundY = 0;
     },
 
     initSprites: function () {
 
         for (let i = 0; i < this.numberOfSprites; i++) {
             let spriteBorn = {
-                positionX: this.width * Math.random(),
-                positionY: this.spriteWidth * 3 * (Math.random() - 1),
-                velocityScale: 0.5 + Math.random() * 1.5,
+                positionX: 0,
+                positionY: 0,
+                velocityScale: 0
             };
+
+            this.replaceSprite(spriteBorn);
 
             this.sprites.push(spriteBorn);
         }
     },
 
-    draw: function (scrollAmount) {
+    initPlayer: function () {
 
-        this.getCanvasSize(this.field);
+        this.playerX = this.width / 2 - this.playerWidth;
+        this.playerY = this.height - this.playerHeight * 2;
+    },
 
+    draw: function (timeInterval) {
+
+        const backgroundVelocity = 600;
+        const spriteVelocity = 360;
+        const playerSpeed = 300;
+
+        this.getSize(this.canvas);
         this.context.clearRect(0, 0, this.width, this.height);
 
         let backgroundImage = this.createBackgroundImage();
 
+        this.scrollBackground(timeInterval / 1000 * backgroundVelocity);
         this.context.drawImage(backgroundImage, 0, this.backgroundY, this.width, this.height);
         this.context.drawImage(backgroundImage, 0, this.backgroundY - this.height, this.width, this.height);
         this.context.drawImage(backgroundImage, 0, this.backgroundY - 2 * this.height, this.width, this.height);
 
-        this.scrollBackground(scrollAmount);
+        let playerImage = this.createPlayerImage();
 
-        let spriteImage;
+        this.movePlayer(timeInterval / 1000 * playerSpeed);
+        this.context.drawImage(playerImage, this.playerX, this.playerY, this.playerWidth, this.playerHeight);
+
         for (let i = 0; i < this.numberOfSprites; i++) {
+            let spriteImage = this.createSpriteImage();
 
-            spriteImage = this.createSpriteImage();
+            this.scrollSprite(this.sprites[i], timeInterval / 1000 * spriteVelocity);
             this.context.drawImage(spriteImage, this.sprites[i].positionX, this.sprites[i].positionY, this.spriteWidth, this.spriteHeight);
 
-            this.scrollSprite(this.sprites[i], scrollAmount);
         }
+        this.timer += timeInterval / 1000;
 
-        // console.log("painter Y: " + this.backgroundY);
+        for (let i = 0; i < this.numberOfSprites; i++) {
+            this.checkCollision(i);
+        }
+    },
+
+    checkCollision: function (index) {
+
+        let sprite = this.sprites[index];
+
+        if (!((sprite.positionX > this.playerX + this.playerWidth) ||
+            (sprite.positionX + this.spriteWidth < this.playerX) ||
+            (sprite.positionY + this.spriteHeight < this.playerY) ||
+            (sprite.positionY > this.playerY + this.playerHeight))) {
+
+            this.sprites.length = 0;
+            stopGame();
+        }
+        // if(sprite.positionX > this.playerX + this.playerWidth){
+        //     console.log("candle's at right");
+        // }
+        // if(sprite.positionX + this.spriteWidth < this.playerX) {
+        //     console.log("candle's at left");
+        // }
+        // if(sprite.positionY + this.spriteHeight < this.playerY) {
+        //     console.log("candle's above");
+        // }
+        // if(sprite.positionY > this.playerY + this.playerHeight) {
+        //     console.log("candle's under");
+        // }
     }
 };
 
-window.onload = painter.initialize();
-setInterval((() => painter.draw(1)), 1000 / 60);
+const keyMap = {};
+
+const directionKey = Object.freeze({
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
+});
+
+const keyEventListener = function (event) {
+
+    if (event.keyCode >= directionKey.LEFT && event.keyCode <= directionKey.DOWN) {
+        event.preventDefault();
+
+        keyMap[event.keyCode] = (event.type == "keydown");
+    }
+};
+
+let intervalId;
+
+const startGame = function () {
+
+    const fps = 60;
+    const refreshInterval = 1000 / fps;
+
+    document.addEventListener("keydown", keyEventListener, false);
+    document.addEventListener("keyup", keyEventListener, false);
+
+    painter.initialize();
+
+    intervalId = setInterval((() => painter.draw(refreshInterval)), refreshInterval);
+};
+
+const stopGame = function () {
+
+    clearInterval(intervalId);
+
+    // document.removeEventListener("keydown",keyEventListener, false);
+    document.addEventListener("keydown", spacebarListener, false);
+    document.removeEventListener("keyup", keyEventListener, false);
+
+    alert("탄핵당했습니다! \n" + painter.timer + " 초를 버텼습니다")
+};
+
+const spacebarListener = function (event) {
+
+    if (event.keyCode == 32) {
+        startGame();
+        event.preventDefault();
+    }
+};
+
+document.addEventListener("keydown", spacebarListener, false);
+
+
 
